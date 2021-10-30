1252c53cb2887bfbc33ba718e0a71b640dee5a71
ByeongUk Choi <cbw0916@gmail.com>
2021-10-30 11:41:44 +0900

    Add di in config

--- config/config.exs
+++ config/config.exs
@@ -25,6 +25,10 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# dependency injection of adapters into ports
+config :blog,
+  article_repo: Blog.ArticleRepo
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
