5c603284c6635ab03bb722682ab7e93a7177fd78
ByeongUk Choi <cbw0916@gmail.com>
2021-11-22 23:10:37 +0900

    Add router

--- lib/blog/adapter/primary/blog_web/router.ex
+++ lib/blog/adapter/primary/blog_web/router.ex
@@ -7,5 +7,22 @@ defmodule BlogWeb.Router do
 
   scope "/api", BlogWeb do
     pipe_through :api
+
+    scope "/articles" do
+      get "/", ArticleController, :index
+      post "/", ArticleController, :create
+      delete "/", ArticleController, :delete
+
+      scope "/:id" do
+        get "/", ArticleController, :show
+        post "/", ArticleController, :update
+
+        scope "/comments" do
+          get "/", CommentController, :index
+          post "/", CommentController, :create
+          post "/:id", CommentController, :update
+        end
+      end
+    end
   end
 end
