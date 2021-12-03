6614fd7fa001498b5cc06632b509516be1702a3a
ByeongUk Choi <cbw0916@gmail.com>
2021-12-04 03:07:17 +0900

    Add article_controller.index/2

--- lib/blog/adapter/primary/blog_web/controllers/article_controller.ex
+++ lib/blog/adapter/primary/blog_web/controllers/article_controller.ex
@@ -3,6 +3,14 @@ defmodule BlogWeb.ArticleController do
 
   alias Blog.Application.ArticleService
 
+  def index(conn, params) do
+    page = Map.get(params, "page", 0)
+    size = Map.get(params, "size", 20)
+    articles = ArticleService.get_articles(page: page, size: size)
+    # TODO: comments
+
+    render(conn, "articles.json", %{articles: articles})
+  end
   def show(conn, %{"id" => article_id}) do
     article = ArticleService.get_article(article_id)
 
